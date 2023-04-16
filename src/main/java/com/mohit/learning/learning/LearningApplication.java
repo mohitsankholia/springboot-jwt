538b10fadb7b859a082731984a02f1ad07e9c051
mohitsankholia <sankholia.mohit@gmail.com>
2023-04-16 11:11:49 +0530

    logger added

--- src/main/java/com/mohit/learning/learning/LearningApplication.java
+++ src/main/java/com/mohit/learning/learning/LearningApplication.java
@@ -1,7 +1,11 @@
 package com.mohit.learning.learning;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mohit.learning.learning.controller.DepartmentController;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -11,7 +15,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 public class LearningApplication {
 
 	public static void main(String[] args) {
+		
+		Logger LOGGER = LogManager.getLogger(LearningApplication.class);
+		
 		SpringApplication.run(LearningApplication.class, args);
+		 
+		LOGGER.info("Application Started at port 8081..!");
 	}
 
 }
